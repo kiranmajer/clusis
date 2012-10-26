6465bc3916cc1890002d7122041a179ffd46c95c
kiran <kiran.majer@physik.uni-freiburg.de>
2012-10-26 18:13:52 +0200

    Hopefully all old files are imported now.

---
+++ cludb/src/spec.py
@@ -0,0 +1,222 @@
+from __future__ import unicode_literals
+from legacyData import *
+import scipy.constants as constants
+from plotlibs import *
+from dbshell import *
+#from recursive_import import *
+from pes_sheet import *
+from msplot import *
+from scipy.optimize import leastsq
+import view
+
+
+
+
+
+class Spec(object):
+    def __init__(self, mdata, xdata, ydata):
+        self.mdata = MdataUtils.Mdata(mdata)
+        self.xdata = xdata
+        self.ydata = ydata
+        
+    
+    
+    def commitDb(self, mdata):
+        Db.add(mdata)
+        
+        
+    def commitPickle(self):
+        '''
+        Stores the self.mdata.data(), self.xdata, self.ydata dicts in a pickle file under the path:
+        config.path['data']/<year>/<recTime>_<sha1>.pickle
+        '''
+        pickleDir = os.path.dirname(self.mdata.data('pickleFile'))
+        if not os.path.exists(pickleDir):
+            os.mkdir(pickleDir)   
+        with open(self.mdata.data('pickleFile'), 'wb') as f:
+            pickle.dump((self.mdata.data(), self.xdata, self.ydata), f)
+            
+            
+    def commit(self):
+        self.commitPickle()
+        self.commitDb(self.mdata.data())
+        
+    '''TODO: make privat'''    
+    def calcTof(self):
+        self.xdata['tof'] = self.xdata['idx']*self.mdata.data('timePerPoint')-self.mdata.data('triggerOffset')-self.mdata.data('timeOffset')
+
+    def photonEnergy(self, waveLength):
+        """Calculates photon energy in eV for a given wave length.
+        
+        Returns: float."""
+        photonEnergy = constants.h*constants.c/(constants.e*waveLength)
+        
+        return photonEnergy
+
+
+        
+
+class peSpec(Spec):
+    def __init__(self, mdata, xdata, ydata):
+        Spec.__init__(self, mdata, xdata, ydata)
+        if len(self.xdata) == 1:
+            self.calcSpec()
+        self.view = view.ViewPes(self)
+    
+    def __calcEkin(self):
+        self.xdata['ekin'] = constants.m_e/(2*constants.e)*(self.mdata.data('flightLength')/self.xdata['tof'])**2
+    
+    def __calcEbin(self):
+        self.xdata['ebin'] = photonEnergy(self.mdata.data('waveLength')) - self.xdata['ekin']
+        
+    def __fixNegIntensities(self):
+        """
+        The Oscilloscope sets the value for bins without counts to the value of the frame of display.
+        So it's safe to set them to 0. 
+        """
+        fixedIntensity = (self.ydata['rawIntensity'] + np.abs(self.ydata['rawIntensity']))/2
+        self.ydata['intensity'] = fixedIntensity
+        
+    def __calcJacobyIntensity(self):
+        self.ydata['jacobyIntensity'] = self.ydata['intensity']/(2*self.xdata['ekin']/self.xdata['tof'])
+    
+    def calcSpec(self):
+        self.calcTof()
+        self.__calcEkin()
+        self.__calcEbin()
+        self.__fixNegIntensities()
+        self.__calcJacobyIntensity()
+        
+        
+        
+class ptSpec(peSpec):
+    def __init__(self, mdata, xdata, ydata):
+        peSpec.__init__(self, mdata, xdata, ydata)
+        self.view = view.ViewPt(self)
+             
+        
+    def __fitPeakPos(self):
+        return [p[0] for p in config.ptPeakPar[self.mdata.data('waveLength')]]
+    
+    
+    def __fitParInit(self, scale, a, b):
+        l = [i for p in config.ptPeakPar[self.mdata.data('waveLength')] for i in [p[1]*scale,p[2]]]
+        l.extend([a,b])
+        return l
+    
+    
+    def __getScale(self, xdata, ydata, xcenter, xinterval):
+        xlb = np.abs(xdata-(xcenter-xinterval/2)).argmin()
+        xub = np.abs(xdata-(xcenter+xinterval/2)).argmin()
+        return ydata[xlb:xub].max()
+    
+    
+    def mGauss(self, x, peak_pos, parList):
+        plist = list(parList)
+        xlist = list(peak_pos)
+        gauss = lambda x, m,A,s,a,b: A*np.exp(-(x-(a*m+b))**2/(2*s**2))
+        b = plist.pop()
+        a = plist.pop()
+        mgauss = 0
+        while len(plist) > 0:
+            s = plist.pop()
+            A = plist.pop()
+            m = xlist.pop()
+            mgauss += gauss(x, m, A, s, a, b)
+        return mgauss
+    
+    
+    def __err_mGauss(self, p,x,y,peak_pos):
+        return self.mGauss(x, peak_pos, p)-y
+    
+    
+    def __fitMgauss(self, a, b):
+        xdata = self.xdata['ebin']
+        ydata = self.ydata['jacobyIntensity']
+        fitValues = {'fitPeakPos': self.__fitPeakPos()}
+        xcenter = fitValues['fitPeakPos'][0]
+        scale = self.__getScale(xdata, ydata, xcenter, 0.2)
+        fitValues['fitPar0'] = self.__fitParInit(scale, a, b)
+        p, covar, info, mess, ierr = leastsq(self.__err_mGauss, fitValues['fitPar0'],args=(xdata,ydata,fitValues['fitPeakPos']), full_output=True)
+        fitValues.update({'fitPar': p, 'fitCovar': covar, 'fitInfo': [info, mess, ierr]})
+        
+        return fitValues
+        
+
+    def gauge(self, a=1, b=0):
+        try:
+            fitValues = self.__fitMgauss( a, b)
+        except:
+            self.mdata.add(fitValues)
+            raise
+        else:
+            self.mdata.add(fitValues)
+
+
+
+class waterSpec(peSpec):
+    def __init__(self, mdata, xdata, ydata):
+        peSpec.__init__(self, mdata, xdata, ydata)
+        self.view = view.ViewWater(self)
+
+
+    def __gl(self, x, xmax, A, sg, sl):
+        y = np.zeros(x.shape)
+        y[x<=xmax] = A*np.exp(-(x[x<=xmax]-xmax)**2/(2*sg**2))
+        y[x>xmax] = A/((x[x>xmax]-xmax)**2/(2*sl**2)+1)
+        return y
+    
+    
+    def mGl(self, x, par):
+        plist = list(par)
+        mgl = 0
+        sl =plist.pop()
+        sg = plist.pop()
+        while len(plist) > 0:
+            A = plist.pop()
+            xmax = plist.pop()
+            mgl+=self.__gl(x, xmax, A, sg, sl)
+        return mgl
+    
+    def __err_mGl(self, par, x, y):
+        return self.mGl(x, par)-y
+    
+    
+    def __fitGl(self, p0, cutoff):
+        xdata = self.xdata['ebin']
+        ydata = np.copy(self.ydata['jacobyIntensity'])
+        fitValues = {'fitPar0': p0, 'fitCutoff': cutoff}
+        if type(cutoff) in [int,float]:
+            ydata[xdata>cutoff] = xdata[xdata>cutoff]*0
+        p, covar, info, mess, ierr = leastsq(self.__err_mGl, p0, args=(xdata,ydata), full_output=True)
+        fitValues.update({'fitPar': p, 'fitCovar': covar, 'fitInfo': [info, mess, ierr]})
+        
+        return fitValues
+    
+    
+    def fit(self, p0, cutoff=None):
+        try:
+            fitValues = self.__fitGl( p0, cutoff)
+        except:
+            self.mdata.add(fitValues)
+            raise
+        else:
+            self.mdata.add(fitValues)
+
+
+
+
+class mSpec(Spec):
+    def calcSpec(self):
+        self.__calcTof()
+        
+        
+
+class pfSpec(Spec):
+    def calcSpec(self):
+        self.__calcTof()
+
+
+
+
+
---
+++ cludb/src/view.py
@@ -0,0 +1,136 @@
+'''
+Created on Sep 4, 2012
+
+@author: kiran
+'''
+import matplotlib.pyplot as plt
+import os.path
+
+
+class View(object):
+    def __init__(self,spec):
+        self.spec = spec
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(1,1,1)
+        
+        
+    def _clearPlot(self):
+        self.ax.lines = []
+        self.ax.texts = []
+            
+            
+    def showIdx(self):
+        self._clearPlot()
+        self.ax.set_xlabel('Index')
+        self.ax.set_ylabel('Intensity (a.u.)')
+        self.ax.set_xlim(0,self.spec.xdata['idx'][-1])
+        self.ax.plot(self.spec.xdata['idx'], self.spec.ydata['intensity'], color='black')
+        self.ax.relim()
+        self.ax.autoscale(axis='y')
+        textId = self.ax.text(1.0, 1.01, '%s'%(os.path.basename(self.spec.mdata.data('datFile'))),
+                                  transform = self.ax.transAxes, fontsize=8, horizontalalignment='right')
+        self.fig.show()
+        
+        
+    def showTof(self, timeUnit=1e-6):
+        '''TODO: adapt for more time units'''
+        self._clearPlot()
+        self.ax.set_xlabel(r'Flight Time ($\mu s$)')
+        self.ax.set_ylabel('Intensity (a.u.)')
+        self.ax.set_xlim(0,self.spec.xdata['tof'][-1]*timeUnit)
+        self.ax.plot(self.spec.xdata['tof']*timeUnit, self.spec.ydata['intensity'], color='black')
+        self.ax.relim()
+        self.ax.autoscale(axis='y')
+        textId = self.ax.text(1.0, 1.01, '%s'%(os.path.basename(self.spec.mdata.data('datFile'))),
+                                  transform = self.ax.transAxes, fontsize=8, horizontalalignment='right')
+        self.fig.show()
+        
+        
+        
+class ViewPes(View):
+    def __init__(self, spec):
+        View.__init__(self, spec)
+
+
+    def showEkin(self):
+        self._clearPlot()
+        self.ax.set_xlabel(r'E$_{kin}$ (eV)')
+        self.ax.set_ylabel('Intensity (a.u.)')
+        self.ax.set_xlim(0,self.spec.photonEnergy(self.spec.mdata.data('waveLength')))
+        self.ax.plot(self.spec.xdata['ekin'], self.spec.ydata['jacobyIntensity'], color='black')
+        self.ax.relim()
+        self.ax.autoscale(axis='y')
+        textId = self.ax.text(1.0, 1.01, '%s'%(os.path.basename(self.spec.mdata.data('datFile'))),
+                                  transform = self.ax.transAxes, fontsize=8, horizontalalignment='right')
+        self.fig.show()
+
+
+    def showEbin(self):
+        self._clearPlot()
+        self.ax.set_xlabel(r'E$_{bin}$ (eV)')
+        self.ax.set_ylabel('Intensity (a.u.)')
+        self.ax.set_xlim(0,self.spec.photonEnergy(self.spec.mdata.data('waveLength')))
+        self.ax.plot(self.spec.xdata['ebin'], self.spec.ydata['jacobyIntensity'], color='black')
+        self.ax.relim()
+        self.ax.autoscale(axis='y')
+        textId = self.ax.text(1.0, 1.01, '%s'%(os.path.basename(self.spec.mdata.data('datFile'))),
+                                  transform = self.ax.transAxes, fontsize=8, horizontalalignment='right')
+        self.fig.show()
+
+
+
+class ViewPt(ViewPes):
+    def __init__(self,spec):
+        ViewPes.__init__(self, spec)
+        
+        
+    def showEbinFit(self, fitPar='fitPar'):
+        self._clearPlot()
+        if fitPar in self.spec.mdata.data().keys():
+            self.showEbin()
+            self.ax.plot(self.spec.xdata['ebin'],
+                         self.spec.mGauss(self.spec.xdata['ebin'],self.spec.mdata.data('fitPeakPos'),self.spec.mdata.data(fitPar)),
+                         color='blue')
+            self.ax.relim()
+            self.ax.autoscale(axis='y')
+            textOffset = self.ax.text(0.05, 0.9, 'Offset: %s meV'%(round(self.spec.mdata.data('fitPar')[-1]*1e3, 2)),
+                                      transform = self.spec.view.ax.transAxes, fontsize=12)
+            textScale = self.ax.text(0.05, 0.85, 'Scale: %s'%(round(self.spec.mdata.data('fitPar')[-2], 2)),
+                                     transform = self.spec.view.ax.transAxes, fontsize=12)
+            self.fig.show()
+        else:
+            raise ValueError('Spectrum not gauged. Gauge first.')
+        
+        
+class ViewWater(ViewPes):
+    def __init__(self,spec):
+        ViewPes.__init__(self, spec)
+        
+        
+    def showEbinFit(self, fitPar='fitPar'):
+        self._clearPlot()
+        if fitPar in self.spec.mdata.data().keys():
+            self.showEbin()
+            self.ax.plot(self.spec.xdata['ebin'],
+                         self.spec.mGl(self.spec.xdata['ebin'], self.spec.mdata.data(fitPar)),
+                         color='blue')
+            self.ax.relim()
+            plist = list(self.spec.mdata.data(fitPar))
+            sl = plist.pop()
+            sg = plist.pop()
+            while len(plist) >= 2:
+                A = plist.pop()
+                xmax = plist.pop()
+                self.ax.plot(self.spec.xdata['ebin'],
+                         self.spec.mGl(self.spec.xdata['ebin'], [xmax,A,sg,sl]),
+                         color='DimGray')
+            self.fig.show()
+        else:
+            raise ValueError('Spectrum not yet fitted. Fit first.')
+
+
+
+
+
+
+        
