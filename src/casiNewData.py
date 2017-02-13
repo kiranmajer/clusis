0b83a3041a2816e02a9278333ffc1c3527ca63ba
Kiran <kiran.majer@physik.uni-freiburg.de>
2017-02-13 08:26:41 +0100

    Added new import class for the new casi data format.

---
+++ src/casiNewData.py
@@ -0,0 +1,35 @@
+import numpy as np
+from hashlib import sha1
+from os.path import splitext
+from os import stat
+
+class CasiNewData():
+    def __init__(self, datafile_path):
+        self.xdata, self.ydata = self.read_data(datafile_path)
+
+
+    def read_data(self, datafile_path):
+        x,y = np.loadtxt(datafile_path,
+                         converters={0: lambda i: float(i.decode('utf').replace(',','.'))},
+                         unpack=True)
+        
+        return x,y
+    
+    
+    def parse_filename(self, fname):
+        y, m, d, element, idx, data_type = splitext(fname)[0].split('_')
+        
+        return int(y), int(m), int(d), element, int(idx), data_type
+    
+    
+    def get_rectime(self, datafile_path):
+        time_stamp = stat(datafile_path).st_mtime
+        
+        return time_stamp
+    
+    
+    def get_sha1(self, datafile_path):
+        with open(datafile_path, 'rb') as f:
+            cksum = sha1(f.read()).hexdigest()
+        
+        return cksum
